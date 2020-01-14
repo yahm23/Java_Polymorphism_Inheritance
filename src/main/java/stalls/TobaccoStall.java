e235cf4b339c650728ecf608135ce3d5f376bedb
Yousef <yahmed1996a@gmail.com>
2020-01-14 15:37:55 +0000

    restrict baccy stall  works

--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,8 +1,22 @@
 package stalls;
 
-public class TobaccoStall extends Stall {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
         super(name, ownerName, parkingSpot);
+    }
+
+
+    public boolean isAllowedToVisit(Visitor visitor) {
+        if (visitor.getAge() < 18) {
+            return false;
+        } else {
+            return true;
+        }
+
+
     }
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,23 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
 
+    Visitor visitor;
+    Visitor visitor2;
+
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+
+        visitor = new Visitor(18, 1.2, 40.0);
+        visitor2 = new Visitor(15, 1.2, 40.0);
     }
 
     @Test
@@ -27,5 +34,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void rejectKidsBuying(){
+        assertTrue(tobaccoStall.isAllowedToVisit(visitor));
+        assertFalse(tobaccoStall.isAllowedToVisit(visitor2));
     }
 }
