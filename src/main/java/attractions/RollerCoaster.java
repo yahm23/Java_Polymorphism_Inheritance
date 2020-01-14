bd0269dc499e398d936d7d40b24d98bc9156b805
Yousef <yahmed1996a@gmail.com>
2020-01-14 15:55:01 +0000

    rollercoaster priced and ticketed

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,9 +1,10 @@
 package attractions;
 
 import behaviours.ISecurity;
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class RollerCoaster extends Attraction implements ISecurity {
+public class RollerCoaster extends Attraction implements ISecurity, ITicketed {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
@@ -15,5 +16,16 @@ public class RollerCoaster extends Attraction implements ISecurity {
         } else {
             return false;
         }
+    }
+
+    public double defaultPrice() {
+        return 8.40;
+    }
+
+    public double priceForVisitor(Visitor visitor) {
+       if (visitor.getHeight()>2.0){
+           return defaultPrice()*2;
+       }
+       else {return defaultPrice();}
     }
 }
